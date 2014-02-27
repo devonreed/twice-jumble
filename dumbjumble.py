a47cc0319edd1ea3153dfea888c85ea2d3077b92
Devon Reed <dreed@admins-MacBook-Pro-34.local>
2014-02-26 21:44:42 -0800

    Dumb Jumble

---
+++ dumbjumble.py
@@ -0,0 +1,21 @@
+def permute(letters, prefix):
+  permutations = list()
+  for idx, letter in enumerate(letters):
+    permutations.append(prefix+letter)
+    newletters = list(letters)
+    del newletters[idx]
+    permutations += permute(newletters, prefix+letter)
+  return permutations
+
+word = raw_input('Jumble Me! (enter a word): ')
+letters = list(word)
+f = open('2of12.txt', 'r')
+possibilities = list()  
+allpermutations = permute(letters, '')
+for dictword in f:
+  dictword = dictword.strip()
+  for permutation in allpermutations:
+    if permutation == dictword:
+      possibilities.append(dictword)
+      break
+print possibilities
